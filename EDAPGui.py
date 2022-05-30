027319116c10b27b45ee8eaeed7353bc95b15790
sumzer0 <sumzer0@yahoo.com>
2022-05-30 08:48:14 -0500

    V1.1.0 Build w/exe

--- EDAPGui.py
+++ EDAPGui.py
@@ -53,7 +53,7 @@ Author: sumzer0@yahoo.com
 # ---------------------------------------------------------------------------
 # must be updated with a new release so that the update check works properly!
 # contains the names of the release.
-EDAP_VERSION = "V1.0 Initial Baseline"
+EDAP_VERSION = "V1.1.0"
 # depending on how release versions are best marked you could also change it to the release tag, see function check_update.
 # ---------------------------------------------------------------------------
 
