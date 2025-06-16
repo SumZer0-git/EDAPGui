7aab485458a4fa9bfb619e2ceefacef2e358495a
Thomas <60296035+Thogronen@users.noreply.github.com>
2025-06-16 22:45:31 +0200

    Updated version :)

--- EDAPGui.py
+++ EDAPGui.py
@@ -66,7 +66,7 @@ Author: sumzer0@yahoo.com
 # ---------------------------------------------------------------------------
 # must be updated with a new release so that the update check works properly!
 # contains the names of the release.
-EDAP_VERSION = "V1.4.2"
+EDAP_VERSION = "V1.4.2 Beta"
 # depending on how release versions are best marked you could also change it to the release tag, see function check_update.
 # ---------------------------------------------------------------------------
 
