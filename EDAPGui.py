8ef8569469c20e3e5058d246adc22ea6e4a34720
Steve Towner <stevetowner@yahoo.com>
2025-07-26 22:27:13 -0500

    Updated change log and version.

--- EDAPGui.py
+++ EDAPGui.py
@@ -55,7 +55,7 @@ Author: sumzer0@yahoo.com
 # ---------------------------------------------------------------------------
 # must be updated with a new release so that the update check works properly!
 # contains the names of the release.
-EDAP_VERSION = "V1.5.0"
+EDAP_VERSION = "V1.6.0"
 # depending on how release versions are best marked you could also change it to the release tag, see function check_update.
 # ---------------------------------------------------------------------------
 
